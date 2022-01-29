8c659ee7055381b6b12907652e36ac925cb3b3a2
TheJoeMaly <59886850+TheJoeMaly@users.noreply.github.com>
2022-01-29 02:57:24 -0500

    adding gradient to opening screen

--- NightLyfe/ContentView.swift
+++ NightLyfe/ContentView.swift
@@ -9,11 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ZStack {
             Text("NightLyfe")
-                .font(.title)
+                .foregroundColor(.white)
+                .font(.title) 
+                
                 .padding()
+                
         }
+        .frame(maxWidth: .infinity,  maxHeight: .infinity)
+        .background(
+              LinearGradient(gradient: Gradient(colors: Color.gradient), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            )
     }
 }
 
@@ -21,4 +29,15 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+extension Color {
+  static var gradient: Array<Color> {
+    return [
+      Color(red: 1/255, green: 120/255, blue: 200/255, opacity: 1.0),
+      Color(red: 0/255, green: 0/255, blue: 0/255, opacity: 1.0),
+//      Color(red: 100/255, green: 100/255, blue: 98/255, opacity: 0.5),
+//      Color(red: 90/255, green: 120/255, blue: 100/255, opacity: 0.2),
+//      Color(red: 50/255, green: 150/255, blue: 100/255, opacity: 1.0)
+    ]
+  }
 }
