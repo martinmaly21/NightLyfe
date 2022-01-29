6747320aacff53fcc822f290223535e991e3421c
Martin Maly <martinmaly66@hotmail.com>
2022-01-29 01:48:18 -0500

    Initial Commit

---
+++ NightLyfe/NightLyfeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightLyfeApp.swift
+//  NightLyfe
+//
+//  Created by Martin Maly on 2022-01-29.
+//
+
+import SwiftUI
+
+@main
+struct NightLyfeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
